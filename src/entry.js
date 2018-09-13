3d3bae9e5a3668253a2834e24ee0479740f67cba
mlzhao <455969765@qq.com>
2018-09-13 15:20:36 +0800

    Initial commit

---
+++ src/entry.js
@@ -0,0 +1,1 @@
+document.getElementById('webpack').innerHTML = "hello webpack!!"
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+module.exports ={
+    entry:{
+        entry:'./src/entry.js',
+        entry2:'./src/entry2.js'
+    },
+    output: {
+        path:path.resolve(__dirname,'dist'),
+        filename:'[name].js'
+    },
+    module: {},
+    plugins: [],
+    devServer: {
+        //webpack开发服务
+        contentBase:path.resolve(__dirname,'dist'),
+        host:'10.5.13.51',
+        compress:true,
+        port:8090
+    }
+}
